1c81db84a1623ee40b5d6cf60b66df3ed0a31c81
khalil moro <MY_NAME@example.com>
2024-01-20 18:40:32 +0100

    third

--- main.js
+++ main.js
@@ -2,18 +2,19 @@ let turn = 0;
 const lista = ["rock", "paper", "scisor"];
 let playerscore = 0;
 let compscore = 0;
-while (turn < 5){
-    startGame();
+// while (turn < 5){
+//     startGame();
 
-}
-alert(`Player score: ${playerscore} Computer score ${compscore} WOHOO`);
-function startGame(){
-    let playerChoice = prompt("choose ").toLowerCase();
+// }
+// alert(`Player score: ${playerscore} Computer score ${compscore} WOHOO`);
+function startGame(playerChoice){
+    // let playerChoice = prompt("choose ").toLowerCase();
     let compChoice = lista[Math.floor(Math.random()*lista.length)];
     if (playerChoice === compChoice){
         startGame();
     }else if (playerChoice === "rock" && compChoice === "scisor"){
-        console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
+        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
+        let 
         playerscore++;
         turn++;
     }else if (playerChoice === "paper" && compChoice === "rock"){
@@ -33,3 +34,17 @@ function startGame(){
     
     
 }
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scisor = document.querySelector(".scisor");
+const div = document.querySelector("results");
+
+rock.addEventListener("click", function () {
+    startGame("rock");
+  });
+paper.addEventListener("click", function () {
+    startGame("paper");
+  });
+scisor.addEventListener("click", function () {
+    startGame("scisor");
+  });;
