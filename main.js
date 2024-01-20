45c3f427221eb0e8f9f2a5ec29a33f321ef53cf3
khalil moro <MY_NAME@example.com>
2024-01-20 19:24:13 +0100

    kk

--- main.js
+++ main.js
@@ -9,27 +9,55 @@ let compscore = 0;
 // alert(`Player score: ${playerscore} Computer score ${compscore} WOHOO`);
 function startGame(playerChoice){
     // let playerChoice = prompt("choose ").toLowerCase();
+    
     let compChoice = lista[Math.floor(Math.random()*lista.length)];
     if (playerChoice === compChoice){
         startGame();
     }else if (playerChoice === "rock" && compChoice === "scisor"){
-        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
-        let 
         playerscore++;
+        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
+        let result = document.createElement("h3");
+        result.innerText = `player choosed ${playerChoice} computer choosed ${compChoice} Player won!
+        Player score: ${playerscore} Computer score: ${compscore}`;
+        div.appendChild(result);
+        
         turn++;
     }else if (playerChoice === "paper" && compChoice === "rock"){
-        console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
         playerscore++;
+        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
+        let result = document.createElement("h3");
+        result.innerText = `player choosed ${playerChoice} computer choosed ${compChoice} Player won!
+        Player score: ${playerscore} Computer score: ${compscore}`;
+        div.appendChild(result);
+        
 
         turn++;
     }else if (playerChoice=== "scisor" && compChoice==="paper"){
-        console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
+        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} Player won!`);
         playerscore++;
+        let result = document.createElement("h3");
+        result.innerText = `player choosed ${playerChoice} computer choosed ${compChoice} Player won!
+        Player score: ${playerscore} Computer score: ${compscore}`;
+        div.appendChild(result);
+        
         turn++;
     }else{
-        console.log(`player choosed ${playerChoice} computer choosed ${compChoice} computer won!`)
+        // console.log(`player choosed ${playerChoice} computer choosed ${compChoice} computer won!`)
         compscore++;
+        let result = document.createElement("h3");
+        result.innerText = `player choosed ${playerChoice} computer choosed ${compChoice} computer won!
+        Player score: ${playerscore} Computer score: ${compscore}`;
+        div.appendChild(result);
+        
         turn++;
+    }
+    if (turn > 4){
+        while (div.firstChild){
+            div.removeChild(div.firstChild);
+        }
+        let end = document.createElement("h2");
+        end.innerText = `Game Over Player score: ${playerscore} Computer score: ${compscore}`;
+        div.appendChild(end);
     }
     
     
@@ -37,14 +65,26 @@ function startGame(playerChoice){
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scisor = document.querySelector(".scisor");
-const div = document.querySelector("results");
+const div = document.querySelector(".results");
 
-rock.addEventListener("click", function () {
+// rock.addEventListener("click", function () {
+//     startGame("rock");
+//   });
+// paper.addEventListener("click", function () {
+//     startGame("paper");
+//   });
+// scisor.addEventListener("click", function () {
+//     startGame("scisor");
+//   });;
+
+
+rock.addEventListener("click", ()=> {
     startGame("rock");
   });
-paper.addEventListener("click", function () {
+paper.addEventListener("click", ()=> {
     startGame("paper");
   });
-scisor.addEventListener("click", function () {
+scisor.addEventListener("click", ()=> {
     startGame("scisor");
-  });;
+  });
+
