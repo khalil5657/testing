b2b637e8468338b40d2c87f7e16fa2ec1bc11cd1
khalil moro <MY_NAME@example.com>
2024-03-24 13:38:52 +0000

    add things

---
+++ calculator.js
@@ -0,0 +1,78 @@
+// const nums = [];
+// const down = document.querySelector(".current");
+// const up = document.querySelector(".result");
+// const dot = document.querySelector(".dot");
+// let result = "";
+// let first_num = "";
+// let theoperator = "";
+// function writeNumbers(num){
+//     // nums.puch(num);
+//     if (num == "."){
+//         if (nums.includes(".") || nums.length == 0){
+//             // dot.disabled = true;
+//         }else{
+//             nums[nums.length] = num;
+//             dot.disabled = true;
+//         }
+//     }else{
+//         nums[nums.length] = num;
+//     }
+//     write();
+
+// }
+// function write(){
+//     let current = nums.join("");
+//     down.textContent = current;
+// }
+
+// function deleteIt(){
+//     nums.pop();
+//     if (! nums.includes(".")){
+//         dot.disabled = false;
+//     }
+//     write();
+// }
+// function clearIt(){
+//     nums.length = 0;
+//     first_num = "";
+//     up.textContent = '';
+//     result = "";
+//     dot.disabled = false;
+//     write();
+// }
+
+// function operator(op){
+//     if (result != ""){
+//         first_num = result;
+//         theoperator = op;
+//         nums.length = 0;
+//         down.textContent = "";
+//     }else if (first_num === ""){
+//         first_num = nums.join("");
+//         theoperator = op;
+//         nums.length = 0;
+//         down.textContent = "";
+//         up.textContent = `${first_num} ${theoperator} `
+//         }
+//     dot.disabled = false;
+// }
+// function equals(){
+//     if (first_num != "" && theoperator != "" && nums.length != 0){
+//     if (theoperator === '/'){
+//         if (Number(nums.join("")) != 0){
+//         result = Number(first_num) / Number(nums.join(""));
+//         up.textConte
+
+
+
+    
+
+function loo(){
+    if (4 > 2){
+        var habla = "khalil"
+    }
+
+}
+loo()
+console.log(hbila)
+console.log(habla)
